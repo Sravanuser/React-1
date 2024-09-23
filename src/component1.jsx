b25272ff7cfee491dfba1f83c2bb910b75c3d7ca
Sravan Kumar <sravanpatel0017@gmail.com>
2024-09-23 13:56:27 +0530

    changed component1

--- src/component1.jsx
+++ src/component1.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
 
 export default function Component1() {
     const firstName = 'emma';
     
   return (
     <div>
-      <h1>{firstName}</h1>
+      <h1>{firstName.toUpperCase()}</h1>
     </div>
   )
 }
